c2e9825c7f44146149b7f1fc1e56c5302d1c0580
askarov044 <dinmuhammed419@gmail.com>
2024-12-23 17:00:07 +0500

    jsx

---
+++ src/widgets/index.js
@@ -0,0 +1,6 @@
+export { Header } from './header/Header';
+export { Footer } from './footer/Footer';
+export { Banner } from './homePage/banner/Banner';
+export { NewArrivals } from './homePage/newArrivals/NewArrivals';
+export { TopSelling } from './homePage/topSelling/TopSelling';
+export { DressStyle } from './homePage/dressStyle/DressStyle';
